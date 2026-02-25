e0bd6713b91d69f8d6c9eb95bbeeb794be360558
DevJuan001 <juanesyt7@gmail.com>
2026-02-25 15:29:35 -0500

    feat: implement authentication login functionality in AuthController and AuthRepository

--- src/main/kotlin/com/example/API/controllers/AuthController.kt
+++ src/main/kotlin/com/example/API/controllers/AuthController.kt
@@ -1,7 +1,19 @@
 package com.example.API.controllers
 
+import com.example.API.models.Login
+import com.example.API.repositories.AuthRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
-@RestController
+@RestController("/auth")
 class AuthController {
+    @Autowired
+    lateinit var authRepository: AuthRepository
+
+    @PostMapping("/login")
+    fun login(@RequestBody login: Login) {
+        return authRepository.login(login)
+    }
 }
--- src/main/kotlin/com/example/API/repositories/AuthRepository.kt
+++ src/main/kotlin/com/example/API/repositories/AuthRepository.kt
@@ -1,7 +1,25 @@
 package com.example.API.repositories
 
+import com.example.API.models.Login
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Repository
 
 @Repository
 class AuthRepository {
+
+    @Autowired
+    lateinit var jdbcTemplate: JdbcTemplate
+
+    fun login(login: Login) {
+        val sql = """
+            SELECT 
+        """.trimIndent()
+
+        jdbcTemplate.update(
+            sql,
+            login.email,
+            login.password
+        )
+    }
 }
