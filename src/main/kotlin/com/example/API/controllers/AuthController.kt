d6320bc0b9b0e9a345a541dc400b73a438a64f54
DevJuan001 <juanesyt7@gmail.com>
2026-02-26 16:28:50 -0500

    fix: correct RestController annotation and update login method return type in AuthController

--- src/main/kotlin/com/example/API/controllers/AuthController.kt
+++ src/main/kotlin/com/example/API/controllers/AuthController.kt
@@ -7,13 +7,13 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
-@RestController("/auth")
+@RestController
 class AuthController {
     @Autowired
     lateinit var authRepository: AuthRepository
 
     @PostMapping("/login")
-    fun login(@RequestBody login: Login) {
+    fun login(@RequestBody login: Login) : Map<String, Any> {
         return authRepository.login(login)
     }
 }
