a1ae76dee08cf4783a49a74882b77c6db8aade9b
EdwinQuenguan <edwinquenguan14@gmail.com>
2026-02-26 12:01:30 -0500

    products

--- src/main/kotlin/com/example/API/controllers/ProductsController.kt
+++ src/main/kotlin/com/example/API/controllers/ProductsController.kt
@@ -1,7 +1,77 @@
 package com.example.API.controllers
 
+import com.example.API.models.Product
+import com.example.API.repositories.ProductsRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class ProductsController {
+    @Autowired
+    lateinit var productsRepository: ProductsRepository
+
+    @GetMapping("/products")
+    fun getProduts(): List<Product> {
+        return productsRepository.getProducts()
+    }
+
+    @PostMapping("/products")
+    fun createProducts(@RequestBody product: Product): Map<String, Any>{
+
+        val createdProduct = productsRepository.createProduct(product)
+
+        return if (createdProduct > 0 ) {
+            mapOf(
+                "success" to true,
+                "message" to "Producto creado correctamente"
+            )
+        } else {
+            mapOf(
+                "success" to false,
+                "message" to "No se pudo crear el producto"
+            )
+        }
+    }
+
+    @PutMapping("/products/{id}")
+    fun updateUser(@RequestBody product: Product, @PathVariable id: Int): Map<String, Any>{
+        val updatedProduct = productRepository.updateProduct(product, id)
+
+        return if (updatedProduct > 0){
+            mapOf(
+                "success" to true,
+                "message" to "Producto actualizado correctamente"
+            )
+        } else {
+            mapOf(
+                "success" to false,
+                "message" to "No se pudo actualizar el usuario"
+            )
+        }
+    }
+
+    @DeleteMapping("/products/{id}")
+    fun deleteProduct(@PathVariable id: Int): Map<String, Any>{
+        val deleteProduct = productRepository.deleteProduct(id)
+
+        return if (deleteProduct > 0){
+            mapOf(
+                "success" to true,
+                "message" to "Producto eliminado correctamente"
+            )
+        } else {
+            mapOf(
+                "success" to false,
+                "message" to "No se pudo eliminar el producto"
+            )
+        }
+    }
+
+
 }
--- src/main/kotlin/com/example/API/repositories/ProductsRepository.kt
+++ src/main/kotlin/com/example/API/repositories/ProductsRepository.kt
@@ -1,7 +1,109 @@
 package com.example.API.repositories
 
+import com.example.API.models.Product
+import com.example.API.models.User
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.core.RowMapper
 import org.springframework.stereotype.Repository
+
 
 @Repository
 class ProductsRepository {
+    @Autowired
+    lateinit var jdbcTemplate: JdbcTemplate
+
+    fun getUsers(): List<User> {
+        return jdbcTemplate.query("SELECT * FROM USERS", RowMapper<User> {rs, _ ->
+            User(
+                rs.getInt("user_id"),
+                rs.getInt("user_id"),
+                rs.getString("user_name"),
+                rs.getString("user_first_surname"),
+                rs.getString("user_second_surname"),
+                rs.getString("user_phone"),
+                rs.getString("user_email"),
+                rs.getString("user_password"),
+                rs.getString("user_address"),
+                rs.getString("user_city"),
+                rs.getString("user_date"),
+            )
+        })
+    }
+
+    fun createUser(user: User): Int {
+        val sql = """
+            INSERT INTO USERS(
+             var input_order_id: Int,
+    var subcategory_id: Int,
+    var product_id : Int,
+    var product_serial: Int,
+    var product_name: String,
+    var product_model: String,
+    var product_stock: Int,
+    var product_garanty_input: String
+            rol_id,
+            user_name,
+            user_first_surname,
+            user_second_surname,
+            user_phone,
+            user_email,
+            user_password,
+            user_address,
+            user_city)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+        """.trimIndent()
+
+        return jdbcTemplate.update(
+            sql,
+            user.rol_id,
+            user.user_name,
+            user.user_first_surname,
+            user.user_second_surname,
+            user.user_phone,
+            user.user_email,
+            user.user_password,
+            user.user_address,
+            user.user_city
+        )
+    }
+
+    fun updateUser(user: User, id: Int): Int {
+        val sql = """
+            UPDATE USERS SET
+                user_name = ?,
+                user_first_surname = ?,
+                user_second_surname = ?,
+                user_phone = ?,
+                user_email = ?,
+                user_password = ?,
+                user_address = ?,
+                user_city = ?
+            WHERE user_id = ?
+        """.trimIndent()
+
+        return jdbcTemplate.update(
+            sql,
+            user.user_name,
+            user.user_first_surname,
+            user.user_second_surname,
+            user.user_phone,
+            user.user_email,
+            user.user_password,
+            user.user_address,
+            user.user_city,
+            id
+        )
+    }
+
+    fun deleteUser(id: Int): Int {
+        val sql = """
+            DELETE FROM USERS
+            WHERE user_id = ?
+        """.trimIndent()
+
+        return jdbcTemplate.update(sql, id)
+    }
+
+
 }
